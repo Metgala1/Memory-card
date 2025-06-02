19c41333a59bd0e965d022ce4b5d02b0a6baf812
ATAGWE ROGER <atagweroger2@gmail.com>
2025-06-02 18:44:35 +0100

    Worked on my header

---
+++ src/component/Header.jsx
@@ -0,0 +1,20 @@
+import '../styles/Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+        <div className="header-div">
+            <h1 className="header-title">Pokemon Memory Game</h1>
+            <div className="score-div">
+                <p className="score">Score: </p>
+                <p className="best-score">Best Score: </p>
+            </div>
+        </div>
+        <p className="rule">
+            Get points by clickin on an image but dont click on any more than once!
+        </p>
+        </div>
+    )
+}
+
+export default Header;
